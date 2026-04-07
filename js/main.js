5980ac70f625b08c8689c8eb4e63a6a1b30fd26c
younessamsong09-hue <Younessamsong09@gmail.com>
2026-04-07 22:48:30 +0100

    إضافة index.html المفقود وتفعيل نظام الصيدليات

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import { initPharmacies } from './modules/pharmacies.js';
+
 document.addEventListener('DOMContentLoaded', () => {
+    console.log("تم تشغيل نظام تاوريرت بنجاح 🚀");
     initPharmacies();
 });
